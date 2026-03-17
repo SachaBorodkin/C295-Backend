f0f0a3aaa0210fb03a282937f7b2f8f0354824f3
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-03-17 13:51:23 +0100

    Step 2

---
+++ api-suivi-eleves-etml/database/factories/student_factory.ts
@@ -0,0 +1,8 @@
+import Factory from '@adonisjs/lucid/factories'
+import Student from '#models/student'
+export const StudentFactory = Factory.define(Student, async ({ faker }) => {
+return {
+name: faker.person.lastName(),
+firstname: faker.person.firstName(),
+}
+}).build()
---
+++ api-suivi-eleves-etml/database/seeders/student_seeder.ts
@@ -0,0 +1,8 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import { StudentFactory } from '#database/factories/student_factory'
+export default class StudentSeeder extends BaseSeeder {
+public async run() {
+// Crée 10 étudiants
+await StudentFactory.createMany(10)
+}
+}
